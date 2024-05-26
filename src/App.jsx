fa73728b177192f67712f68442c6c439157e707b
Suresh Choudhary <sureshchoudhary8636@gmail.com>
2024-05-26 21:46:56 +0530

    Added Navbar and Login and Logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
+
+  return !loading ? (
     <>
-      <h1>Blog App With AppWrite</h1>
+      <div className="min-h-screen flex flex-wrap justify-between bg-gray-400">
+        <div className="w-full">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  );
+  ) : null;
 }
 
 export default App;
