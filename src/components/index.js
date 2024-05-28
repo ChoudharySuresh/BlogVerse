13d8ec01aaabbe431d5c37f693576a8b92476a1b
Suresh Choudhary <sureshchoudhary8636@gmail.com>
2024-05-28 22:44:24 +0530

    Added Login , signUp form with protected routes

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 
-export { Header, Footer, Container, Logo, Button, LogoutBtn };
+export { Header, Footer, Container, Logo, Button, LogoutBtn, Input };
