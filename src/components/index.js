d7a93b4d63d9e83e95676ce5286871ca9feee4ce
Suresh Choudhary <sureshchoudhary8636@gmail.com>
2024-06-02 23:11:54 +0530

    Added All Pages & Implemented Routing

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,24 @@ import Button from "./Button";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import PostForm from "./post-form/PostForm";
 
-export { Header, Footer, Container, Logo, Button, LogoutBtn, Input, RTE };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  Button,
+  LogoutBtn,
+  Input,
+  RTE,
+  SignUp,
+  Login,
+  PostCard,
+  AuthLayout,
+  PostForm,
+};
